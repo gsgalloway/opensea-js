5297690a4a4d7bb96e693cda206d48613f00561c
Gavi Galloway <gsgalloway@users.noreply.github.com>
2021-11-16 09:18:34 -0800

    fulfillOrderTxData

--- src/seaport.ts
+++ src/seaport.ts
@@ -875,6 +875,26 @@ export class OpenSeaPort {
     return transactionHash
   }
 
+  public async fulfillOrderTxData(
+    { order, accountAddress, recipientAddress, referrerAddress }:
+    { order: Order;
+        accountAddress: string;
+        recipientAddress?: string;
+        referrerAddress?: string; }
+    ): Promise<string> {
+    const matchingOrder = this._makeMatchingOrder({
+      order,
+      accountAddress,
+      recipientAddress: recipientAddress || accountAddress
+    })
+
+    const { buy, sell } = assignOrdersToSides(order, matchingOrder)
+
+    const metadata = this._getMetadata(order, referrerAddress)
+    const transactionData = await this._atomicMatchTxData({ buy, sell, accountAddress, metadata })
+    return transactionData
+  }
+
   /**
    * Cancel an order on-chain, preventing it from ever being fulfilled.
    * @param param0 __namedParameters Object
@@ -2870,6 +2890,65 @@ export class OpenSeaPort {
     return undefined
   }
 
+  private async _atomicMatchTxData(
+    { buy, sell, accountAddress, metadata = NULL_BLOCK_HASH }:
+    { buy: Order; sell: Order; accountAddress: string; metadata?: string }
+  ): Promise<string> {
+    let value
+    let shouldValidateBuy = true
+    let shouldValidateSell = true
+
+    if (sell.maker.toLowerCase() == accountAddress.toLowerCase()) {
+      // USER IS THE SELLER, only validate the buy order
+      await this._sellOrderValidationAndApprovals({ order: sell, accountAddress })
+      shouldValidateSell = false
+
+    } else if (buy.maker.toLowerCase() == accountAddress.toLowerCase()) {
+      // USER IS THE BUYER, only validate the sell order
+      await this._buyOrderValidationAndApprovals({ order: buy, counterOrder: sell, accountAddress })
+      shouldValidateBuy = false
+
+      // If using ETH to pay, set the value of the transaction to the current price
+      if (buy.paymentToken == NULL_ADDRESS) {
+        value = await this._getRequiredAmountForTakingSellOrder(sell)
+      }
+    } else {
+      // User is neither - matching service
+    }
+
+    await this._validateMatch({ buy, sell, accountAddress, shouldValidateBuy, shouldValidateSell })
+
+    this._dispatch(EventType.MatchOrders, { buy, sell, accountAddress, matchMetadata: metadata })
+
+    let txHash
+    const txnData: any = { from: accountAddress, value }
+    const args: WyvernAtomicMatchParameters = [
+      [buy.exchange, buy.maker, buy.taker, buy.feeRecipient, buy.target,
+      buy.staticTarget, buy.paymentToken, sell.exchange, sell.maker, sell.taker, sell.feeRecipient, sell.target, sell.staticTarget, sell.paymentToken],
+      [buy.makerRelayerFee, buy.takerRelayerFee, buy.makerProtocolFee, buy.takerProtocolFee, buy.basePrice, buy.extra, buy.listingTime, buy.expirationTime, buy.salt, sell.makerRelayerFee, sell.takerRelayerFee, sell.makerProtocolFee, sell.takerProtocolFee, sell.basePrice, sell.extra, sell.listingTime, sell.expirationTime, sell.salt],
+      [buy.feeMethod, buy.side, buy.saleKind, buy.howToCall, sell.feeMethod, sell.side, sell.saleKind, sell.howToCall],
+      buy.calldata,
+      sell.calldata,
+      buy.replacementPattern,
+      sell.replacementPattern,
+      buy.staticExtradata,
+      sell.staticExtradata,
+      [
+        buy.v || 0,
+        sell.v || 0
+      ],
+      [
+        buy.r || NULL_BLOCK_HASH,
+        buy.s || NULL_BLOCK_HASH,
+        sell.r || NULL_BLOCK_HASH,
+        sell.s || NULL_BLOCK_HASH,
+        metadata
+      ]
+    ]
+
+   return this._wyvernProtocol.wyvernExchange.atomicMatch_.getABIEncodedTransactionData(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10])
+  }
+
   private async _atomicMatch(
       { buy, sell, accountAddress, metadata = NULL_BLOCK_HASH }:
       { buy: Order; sell: Order; accountAddress: string; metadata?: string }
@@ -2941,6 +3020,7 @@ export class OpenSeaPort {
     // Then do the transaction
     try {
       this.logger(`Fulfilling order with gas set to ${txnData.gas}`)
+      this._wyvernProtocol.wyvernExchange.atomicMatch_.getABIEncodedTransactionData(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10])
       txHash = await this._wyvernProtocol.wyvernExchange.atomicMatch_.sendTransactionAsync(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], txnData)
     } catch (error) {
       console.error(error)
